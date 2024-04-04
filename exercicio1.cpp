8492626912301a8c12f3715f313af43be64b9bbf
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-04 23:19:40 +0000

    feito

---
+++ exercicio1.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+int main(){
+
+    int num;
+
+    printf("Digite o numero: ");
+    scanf("%d", &num);
+
+    if (num > 10){
+        printf("o numero digitado é maior que 10.\n");
+    }
+
+    return 0;
+}
---
+++ exercicio2.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+int main(){
+
+    int num;
+
+    printf("Digite o numero: ");
+    scanf("%d", &num);
+
+    if (num == 0){
+        printf("o numero digitado é igual a zero .\n");
+    }
+
+    return 0;
+}
